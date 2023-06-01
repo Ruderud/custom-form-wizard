d12160ab2a35d92ba7d82be4ecbbc0188f3eb686
정경훈 <ruderud0055@gmail.com>
2023-06-01 16:44:03 +0900

    feat: route 수정, 컴포넌트 이동

--- src/app/onboarding/user-info/page.tsx
+++ src/app/onboarding/user-info/page.tsx
@@ -1,12 +1,13 @@
 import Image from 'next/image';
-import { ScrollSnapFormSlider } from './ScrollSnapFormSlider';
+import { ScrollSnapFormSlider } from '../../../component/client/ScrollSnapFormSlider';
+import Link from 'next/link';
 
 export const runtime = 'edge';
 
 export default function UserInfo() {
   return (
     <main className="p-10">
-      <button className="text-base">Back to Main</button>
+      <Link href="/">Back to Main</Link>
       <div className="text-lg color:red">Basic User Info</div>
       <Image src="/icons/check.svg" alt="Check" className="dark:invert" width={24} height={24} priority />
 
